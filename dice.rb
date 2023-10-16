f664e1d089a1eacf0f6fb09a777104af19227e25
Josh T <63258315+jtangs-tk@users.noreply.github.com>
2023-10-16 17:21:06 +0000

    changes:

--- dice.rb
+++ dice.rb
@@ -1,7 +1,76 @@
 # /dice.rb
-
+require "sinatra/reloader"
 require "sinatra"
 
+
+get("/") do
+  <<-HTML
+  <h1>Dice Roll</h1>
+  
+  <div><a href="/dice/2/6">Roll two 6-sided dice</a></div>
+
+  <div><a href="/dice/2/10">Roll two 10-sided dice</a></div>
+
+  <div><a href="/dice/1/20">Roll a 20-sided die</a></div>
+
+  <div><a href="/dice/5/4">Roll five 4-sided dice</a></div>
+  HTML
+end
+
+
 get("/zebra") do
-  "Hello World"
+  "We must add a route for each path we want to support"
+end
+
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+	
+  outcome = "You rolled a #{first_die} for a total of #{first_die}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+	second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+	
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+	
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
 end
