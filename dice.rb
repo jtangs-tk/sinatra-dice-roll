30fcb3db0222f3f404b60914b2647a562c444849
Josh T <63258315+jtangs-tk@users.noreply.github.com>
2023-10-16 17:27:29 +0000

    changes

--- dice.rb
+++ dice.rb
@@ -1,6 +1,13 @@
 # /dice.rb
 require "sinatra/reloader"
 require "sinatra"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
 get("/") do
